8e5da511cfd203cc5894550df2d5e36bca7f1ee3
ysm <clov7153@gmail.com>
2025-06-29 00:42:26 +0900

    관리자 계정 로그인, 인프라 테스트

--- economy/src/main/java/com/at_a_look/economy/EconomyApplication.java
+++ economy/src/main/java/com/at_a_look/economy/EconomyApplication.java
@@ -11,5 +11,5 @@ public class EconomyApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EconomyApplication.class, args);
 	}
-	// login, infra testing
+	// login, infra testing.
 }
